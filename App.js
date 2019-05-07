923983dc03a5bfb73f0fd9f421a8bd45c48b0a23
LorandoZ <15999868+LorandoZ@users.noreply.github.com>
2019-05-07 19:04:57 +0800

    CreateExampleNavigator

--- App.js
+++ App.js
@@ -1,24 +1,89 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from "react-navigation";
 import { Button, ThemeProvider } from 'react-native-elements';
 
-class HomeScreen extends React.Component {
+class StudyTimerScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ThemeProvider>
-          <Button title="Hey!" />
-        </ThemeProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>StudyTimerScreen</Text>
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
+class LunchTimerScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>LunchTimerScreen</Text>
+      </View>
+    );
   }
-});
+}
 
-export default createAppContainer(AppNavigator);
+class SleepTimerScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>SleepTimerScreen</Text>
+      </View>
+    );
+  }
+}
+
+class StudyStatisticsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>StudyStatisticsScreen</Text>
+      </View>
+    );
+  }
+}
+
+class LunchStatisticsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>LunchStatisticsScreen</Text>
+      </View>
+    );
+  }
+}
+
+class SleepStatisticsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>SleepStatisticsScreen</Text>
+      </View>
+    );
+  }
+}
+
+const TopTimerNavigator = createMaterialTopTabNavigator(
+  {
+    Study: StudyTimerScreen,
+    Lunch: LunchTimerScreen,
+    Sleep: SleepTimerScreen,
+  }
+);
+
+const TopStatisticsNavigator = createMaterialTopTabNavigator(
+  {
+    Study: StudyStatisticsScreen,
+    Lunch: LunchStatisticsScreen,
+    Sleep: SleepStatisticsScreen,
+  }
+);
+
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Timer: TopTimerNavigator,
+    Statistics: TopStatisticsNavigator,
+  }
+);
+
+export default createAppContainer(BottomTabNavigator);
